a26ab1c9a431ec980a993393eba4b048c13d2014
shreeyakapoor190 <shreeyakapoor190@gmail.com>
2021-09-24 03:07:49 +0530

    commit changes

---
+++ 1684.count-the-number-of-consistent-strings.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1684 lang=cpp
+ *
+ * [1684] Count the Number of Consistent Strings
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int countConsistentStrings(string allowed, vector<string> &words)
+    {
+        int l =
+    }
+};
+// @lc code=end
