b5eb3d6f5e2c426adeea889f9aef8313fecb608f
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2021-11-09 00:07:39 +0530

    Time: 8 ms (97.06%), Space: 9.8 MB (99.78%) - LeetHub

---
+++ destination-city/destination-city.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+       int i, j, l = paths.size(), flag;
+        for (i = 0; i < l; i++)
+        {
+            flag = 0;
+            for (j = 0; j < l; j++)
+            {
+                if (paths[i][1] == paths[j][0])
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                return paths[i][1];
+            }
+        }
+        return NULL;
+    }
+};
