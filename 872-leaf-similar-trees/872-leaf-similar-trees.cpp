98f47f41bcb62755959799815db151203e9b4870
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-02-05 22:03:47 +0530

    Time: 17 ms (5.99%), Space: 18.2 MB (5.09%) - LeetHub

---
+++ 872-leaf-similar-trees/872-leaf-similar-trees.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> v1,v2;
+    vector<int> l;
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        
+        v1 = leaf(root1);
+        l.clear();
+        v2 = leaf(root2);
+        if(v1.size()!= v2.size())
+            return false;
+        int i;
+        for(i= 0; i<v1.size(); i++)
+        {
+            if(v1[i] != v2[i])
+                return false;
+        }
+        return true;
+    }
+    
+    vector<int> leaf(TreeNode *root)
+    {
+    
+        if(root)
+        {
+            leaf(root->left);
+            if(root->left == NULL && root->right == NULL)
+                l.push_back(root->val);
+            leaf(root->right);
+        }
+        return l;
+    }
+};
