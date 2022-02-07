5346fac59e5d54006eaf4bd51939fe0f566c379e
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-02-07 13:54:15 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (25.35%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int> s1, t1;
+        int i;
+        for(i=0; i<s.length(); i++)
+        {
+            s1[s[i]]++;
+        }
+        for(i=0; i<t.length(); i++)
+        {
+            t1[t[i]]++;
+        }
+        for(i=0; i<s.length(); i++)
+        {
+            t1[s[i]]--;
+            if(t1[s[i]]==0)
+                t1.erase(s[i]);
+        }
+        for(auto x:t1)
+            return x.first;
+        return 'a';
+    }
+};
