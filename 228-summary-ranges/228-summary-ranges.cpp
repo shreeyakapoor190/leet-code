9ff027ae5391dbb5005c4f4811f2f8ac86dfb1c6
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-03-02 00:02:53 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (61.88%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -6,28 +6,21 @@ public:
         vector<string> result;
         for(i=0;i<nums.size();i++)
         {
-           
+            r="";
             j=i;
             while(j+1 < nums.size() && nums[j]+1 == nums[j+1] )
             {
                 j++;
-                // c++;
             }
             if(j>i){
-               
-                // cout<<nums[i]<<" "<<nums[j]<<" ";
-                
                 r+= to_string(nums[i]) + "->" + to_string(nums[j]);
-                 
             }
             else{
-                // cout<<nums[i]<<" ";
                 r+= to_string(nums[i]);
-                // result.push_back());
             }
             result.push_back(r);
             i=j;
-             r="";
+           
         }
         return result;
     }
