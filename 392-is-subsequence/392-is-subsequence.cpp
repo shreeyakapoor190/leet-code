00702f6dad405c55270c0412affeae0029ed8509
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-03-02 18:49:29 +0530

    Time: 6 ms (23.26%), Space: 6.2 MB (99.09%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i,j, tl= t.size(), sl=s.size();
-       j=0;
-        for(i=0;i<tl; i++)
+        int i,j=0;
+        for(i=0;i< t.size(); i++)
        {
            if(t[i]==s[j])
                j++;
         }
-        return(j==sl);
+        return(j==s.size());
       }
 };
