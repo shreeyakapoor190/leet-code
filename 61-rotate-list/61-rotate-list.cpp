ca38d0c65d0acd908f2b2cf38042061a9e45de13
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-03-12 12:06:27 +0530

    Time: 10 ms (55.14%), Space: 11.8 MB (55.94%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -28,8 +28,8 @@ public:
         ListNode* second;
         while(count!= size-1)
         {
-            if(count == size-k-1)  //storing the start of 2nd part of linked list from where we have to break.
-                second= curr;   //4 5
+            if(count == size-k-1)  //storing the start of 2nd part of linked list from where we                                             have to break.
+                second= curr;   //4 5 (adress to 4)
         count++;
         curr= curr->next;
         }
@@ -38,36 +38,6 @@ public:
         curr->next = head;  //45123 (curr was 5 after the  end of while loop)
         head= temp;
         return head;
-         // if(head==NULL||head->next==NULL){
-//             return head;
-//         }
-
-//         ListNode* curr=head;
-//         int c=0;
-//         while(curr!=NULL){
-//             curr=curr->next;
-//             c++;
-//         }
-//         k=k%c;
-//         if(k==0){
-//             return head;
-//         }
-//         curr=head;
-//         int count=c;
-//         c=0;
-//         ListNode* main;
-//         while(c!=count-1){
-//             if(c==count-k-1){
-//                 main=curr;
-//             }
-//             c++;
-//             curr=curr->next;
-            
-//         }
-//         ListNode * temp=main->next;
-//         main->next=NULL;
-//         curr->next=head;
-//         head=temp;
-//         return head;
+        
     }
 };
