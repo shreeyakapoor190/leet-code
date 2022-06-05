1586756b0a4ed496039e007eae3750771e2796b8
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-06-06 00:33:53 +0530

    Added solution - LeetHub

---
+++ Pairs with specific difference - GFG/pairs-with-specific-difference.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    int maxSumPairWithDifferenceLessThanK(int arr[], int N, int K)
+    {
+        // Your code goes here   
+        int n=sizeof(arr);
+        sort(arr,arr+N);
+        int sum=0;
+        for(int i=N-2;i>=0;i--){
+            if(arr[i+1]-arr[i]<K){
+                sum=sum+arr[i+1]+arr[i];
+                i--;
+            }
+        }
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int N;
+		cin>>N;
+		int arr[N];
+		for(int i=0;i<N;i++)
+		cin>>arr[i];
+
+		int K;
+		cin>>K;
+        Solution ob;
+		cout<<ob.maxSumPairWithDifferenceLessThanK(arr,N,K)<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
