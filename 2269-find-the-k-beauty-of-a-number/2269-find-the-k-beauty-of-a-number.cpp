b772d98215908459b21cae112e016984eec1b951
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-06-09 02:37:39 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (99.38%) - LeetHub

---
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
+          string s="";
+        string m=to_string(num);
+        int c=0;
+        for(int i=0;i<m.length()-k+1;i++){
+            for(int j=i; j<i+k ; j++){
+                s=s+m[j];
+            }
+            int n=stoi(s);      //convert string to integer
+            if(n!=0&&num%n==0){
+                c++;
+            }
+            s="";
+        }
+        return c;
+    }
+};
