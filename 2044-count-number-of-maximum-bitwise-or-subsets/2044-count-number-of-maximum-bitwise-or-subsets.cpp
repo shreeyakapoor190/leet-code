24b72d65e4e3afbbb28851938676a34b30fea4b5
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-06-18 00:43:54 +0530

    Time: 47 ms (59.18%), Space: 8.4 MB (83.22%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int sum=0;
+    int countMaxOrSubsets(vector<int>& nums) {
+        int m=INT_MIN, count=0;
+         int n = nums.size();
+        int curr_xor = 0, sum=0;
+        
+        findMax(nums, n, 0, curr_xor, m);
+        sum=0;
+        countMax(nums, m, count, n, 0, curr_xor );
+        // cout<<m;
+        return count;
+    }
+    void findMax( vector<int> &nums, int n, int i, int curr_xor, int &m )
+    {
+        if(i==n){
+            // sum = sum|curr_xor;    //reached null of one subset 
+            // cout<<sum<<endl;
+            m= max(m, curr_xor);
+            return;
+        }
+        //2 cases possible
+        findMax(nums, n, i+1, curr_xor|nums[i], m);  //including XOR of nums[i]
+        findMax(nums, n, i+1, curr_xor, m);          //excluding XOR of nums[i]
+    
+    }
+    void countMax(vector<int> &nums, int &m, int &count, int n, int i, int curr_xor)
+    {
+        if(i==n)
+        {
+            // sum= sum|curr_xor;
+            if(curr_xor== m)
+                count++;
+            return;
+        }
+        countMax(nums, m, count, n, i+1, curr_xor|nums[i]);
+        countMax(nums, m, count, n, i+1, curr_xor);
+    }
+};
