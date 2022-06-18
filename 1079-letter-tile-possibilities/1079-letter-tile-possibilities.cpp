17f5e59695dec8aff023d2a1d3265a15901c2745
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-06-18 23:07:23 +0530

    Time: 71 ms (61.45%), Space: 17.6 MB (18.04%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int numTilePossibilities(string tiles) {
+        vector<int> freq;
+        string res;
+        freq.resize(tiles.length(), 0);
+        int count=0;
+        unordered_map<string, int> m;
+        solve(tiles, res, freq, count, m);
+        return count-1;
+    }
+    void solve(string tiles, string res, vector<int> & freq, int &count, unordered_map<string, int> &m)
+    {
+        if(res.length()<tiles.length())
+        {
+            
+            if(m.find(res)==m.end())
+            {
+                // cout<<res<<endl;
+                count++;
+                m[res]++;
+            } 
+            
+        }
+        else if(res.length()==tiles.length())
+        { if(m.find(res)==m.end())
+            {
+                // cout<<res<<endl;
+                count++;
+                m[res]++;
+                
+            } 
+            return;
+        }
+       
+        for(int i=0; i<tiles.length(); i++)
+        {
+            if(freq[i]==0)
+            {
+               res+=tiles[i];
+                freq[i]= 1;
+                solve(tiles, res, freq, count, m);
+                freq[i]=0;
+                res.pop_back();
+            }
+        }
+        return;
+    }
+   
+};
