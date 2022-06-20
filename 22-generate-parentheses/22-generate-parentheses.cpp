d12b23253b70473d4a3f40a01103071e6407f1a6
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-06-21 01:26:14 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (86.98%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        int l=n, r=n;
+        string s;
+        solve(res, s, l, r);
+        return res;
+    }
+    void solve(vector<string> &res, string &s, int l, int r)
+    {
+        if(l==0 && r==0)
+        {
+            res.push_back(s);
+            return;
+        }
+        
+        if(l>0)
+        {
+            s.push_back('(');
+            solve(res, s, l-1, r);
+            s.pop_back();
+        }
+        if(r>l)
+        {
+            s.push_back(')');
+            solve(res, s, l, r-1);
+            s.pop_back();
+        }
+    }
+};
