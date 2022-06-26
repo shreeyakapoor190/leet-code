1952745ea88c979764ccbc42c3ef07688c864c69
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-06-26 21:01:11 +0530

    Time: 30 ms (5.13%), Space: 13.9 MB (27.59%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -7,16 +7,7 @@ public:
     }
     int solve(vector<int> &cost, int n)
     {
-//         if(n==0)
-//             return cost[0];
-//         if(n==1)
-//             return cost[1];
-        
-//         if(dp[n] != -1)
-//             return dp[n];
-//         dp[n]= cost[n] + min(solve(cost, n-1, dp), solve(cost, n-2, dp));
-//         return dp[n];
-        vector<int>dp(n+1);
+        vector<int>dp(n);
         dp[0]= cost[0];
         dp[1]= cost[1];
         for(int i=2; i<n; i++)
