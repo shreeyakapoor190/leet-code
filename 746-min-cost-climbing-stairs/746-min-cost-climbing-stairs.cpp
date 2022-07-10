b42cbaa1762b5558fcdded5b6aadcab5074206ce
Shreeya Kapoor <shreeyakapoor190@gmail.com>
2022-07-10 12:00:29 +0530

    Time: 12 ms (42.10%), Space: 13.9 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -8,6 +8,7 @@ public:
     int solve(vector<int> &cost, int n)
     {
         vector<int>dp;
+        //in this dp array we store the min to reach each stair from 0 to n
         dp.push_back(cost[0]);
         dp.push_back(cost[1]);
         for(int i=2; i<n; i++)
